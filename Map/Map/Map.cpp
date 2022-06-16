8399e0e4a3591cae20c99c310cc7b254e2af9627
Winstone <winstone1108@naver.com>
2022-06-16 12:51:15 +0900

    ContainerMap v0.2

--- Map/Map/Map.cpp
+++ Map/Map/Map.cpp
@@ -1,4 +1,4 @@
-// ** ContainerMap v0.1
+// ** ContainerMap v0.2
 #include <iostream>
 #include <string>
 #include <map>
@@ -6,98 +6,40 @@
 using namespace std;
 
 
+map<int, int> Numbers;
+
+
+
+void AddData(int _Key, int _Value);
+
 
 int main(void)
 {
-	/*
-	map<int, int> Numbers;
 
-	// ** Key, Value
-	//map<int, int>
+	Numbers[0] = 0;
+	Numbers[1] = 10;
+	Numbers[2] = 20;
+	Numbers.insert(make_pair(3, 30));
 
-	Numbers[0] = 1;
-	Numbers[1] = 2;
-	Numbers[2] = 3;
-	Numbers[3] = 4;
-	Numbers[4] = 5;
-	*/
+	Numbers[1] = 100;
 
+	Numbers.insert(make_pair(4, 40));
 
-
-
-
-	/*
-	map<string, int> Numbers;
-
-	// ** Key, Value
-	//map<int, int>
-
-	Numbers["A"] = 1;
-	Numbers["B"] = 2;
-	Numbers["C"] = 3;
-	Numbers["D"] = 4;
-	Numbers["E"] = 5;
-
-	//for (map<string, int>::iterator iter = Numbers.begin();
-	//	iter != Numbers.end(); ++iter)
-	//{
-	//	cout << iter->first << endl;
-	//	cout << iter->second << endl << endl;
-	//}
-
-	for (auto iter = Numbers.begin(); iter != Numbers.end(); ++iter)
-	{
-		cout << iter->first << endl;
-		cout << iter->second << endl << endl;
-	}	
-	*/
-
-
-
-
-
-	/*
-	enum Key { AAA, BBB, CCC, DDD, EEE };
-
-	const int Max = 10;
-	int max = 10;
-
-	Key _State = AAA;
-	int Array[EEE];
-
-	cout << Array[_State] << endl;
-	*/
-
-
-
-
-
-
-	enum Key 
-	{ 
-		Player, 
-		Enemy, 
-		Bullet, 
-		Max,
-	};
-
-	const int MaxObject = 128;
-
-	int Array[Max][MaxObject];
-
-	for (int i = 0; i < Max; ++i)
-	{
-		for (int j = 0; j < MaxObject; ++j)
-		{
-			Array[i][j] = j;
-		}
-	}
-
-	for (int i = 0; i < MaxObject; ++i)
-		cout << Array[Enemy][i] << endl;
-
-
-
+	AddData(2, 200);
+	
+	for (map<int, int>::iterator iter = Numbers.begin();
+		iter != Numbers.end() ; ++iter)
+		cout << iter->second << endl;
 
 	return 0;
+}
+
+void AddData(int _Key, int _Value)
+{
+	map<int, int>::iterator iter = Numbers.find(_Key);
+
+	if (iter == Numbers.end())
+		Numbers.insert(make_pair(_Key, _Value));
+	else
+		iter->second = _Value;
 }
